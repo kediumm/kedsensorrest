0fef543e9ff4aa712e1cd8f8b8976715bd119d7f
Miguel Vale Teixeira <miguel@kediumm.com>
2017-01-26 12:51:10 +0000

    create DAO to CRUD operations for AirQuality data
    Signed-off-by: Miguel Vale Teixeira <miguel@kediumm.com>

---
+++ src/main/java/com/kediumm/kedsensorrest/controller/dao/AirQualityDAO.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2017 Miguel Vale Teixeira
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author: Miguel Vale Teixeira
+ * @data: 2017/01/21
+ *
+ * This source code is intended for educational and illustrative purposes only
+ * and is provided AS-IS without warranty of any kind.
+ *
+ */
+package com.kediumm.kedsensorrest.controller.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.kediumm.kedsensorrest.model.AirQuality;
+
+
+/**
+ * Data Access Object CRUD operations for AirQuality 
+ *
+ */
+@XmlRootElement
+@XmlSeeAlso(AirQuality.class)
+public class AirQualityDAO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private static final AirQualityDAO instance = new AirQualityDAO();
+	
+	private SessionFactory sessionFactory;
+
+	private Session session;
+	
+	private AirQualityDAO() {
+		
+		sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(AirQuality.class)
+				.buildSessionFactory();
+
+	}
+	
+	/**
+	 * AirQualityDAO Singleton Pattern 
+	 * @return AirQualityDAO instance
+	 */
+	public static AirQualityDAO getInstance() {
+		
+		return instance;
+	}
+	
+	/**
+	 * Get all records of AirQuality 
+	 * @return AirQuality list
+	 */
+	@SuppressWarnings("unchecked")
+	@XmlElement(name = "AllAirQualities")
+	public List<AirQuality> getAll() {
+		
+		try {
+			session = sessionFactory.getCurrentSession();
+			
+			session.beginTransaction();
+			
+			List<AirQuality> lst_airqlt = session.createQuery("from AirQuality").getResultList();
+			
+			session.getTransaction().commit();
+			
+			return lst_airqlt;
+		
+		} finally {
+			
+			session.close();
+		}
+	}
+
+}
