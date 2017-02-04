3dfc9837c9d48908e544ccf327742f11c9b2f2ce
Miguel Vale Teixeira <miguel@kediumm.com>
2017-02-04 19:27:24 +0000

    add Restful WS method to update AirQuality record
    Signed-off-by: Miguel Vale Teixeira <miguel@kediumm.com>

--- src/main/java/com/kediumm/kedsensorrest/controller/ws/restful/AirQualityRestWS.java
+++ src/main/java/com/kediumm/kedsensorrest/controller/ws/restful/AirQualityRestWS.java
@@ -30,6 +30,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -110,4 +111,23 @@ public class AirQualityRestWS {
 		
 		return Response.ok().build();
 	}
+	
+	/**
+	 * Update new AirQuality record
+	 * @param airQuality
+	 * @return
+	 */
+	@PUT
+	public Response update(AirQuality airQuality) {
+		
+		if ( airQuality == null ) {
+			
+			throw new BadRequestException();
+		}
+		
+		_airquality.updateAirQuality(airQuality);
+		
+		return Response.ok().build();
+	}
+	
 }
