f327b4bcbc7f7b1e909e8501ea0e09f7d70437a0
Miguel Vale Teixeira <miguel@kediumm.com>
2017-02-04 20:37:00 +0000

    add Restful WS method to update TrafficFlow record
    Signed-off-by: Miguel Vale Teixeira <miguel@kediumm.com>

--- src/main/java/com/kediumm/kedsensorrest/controller/ws/restful/TrafficFlowRestWS.java
+++ src/main/java/com/kediumm/kedsensorrest/controller/ws/restful/TrafficFlowRestWS.java
@@ -30,6 +30,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -105,6 +106,24 @@ public class TrafficFlowRestWS {
 		}
 		
 		_trafficflow.createTrafficFlow(trafficFlow);
+		
+		return Response.ok().build();
+	}
+	
+	/**
+	 * Update new TrafficFlow record
+	 * @param trafficFlow
+	 * @return
+	 */
+	@PUT
+	public Response update(TrafficFlow trafficFlow) {
+		
+		if ( trafficFlow == null ) {
+			
+			throw new BadRequestException();
+		}
+		
+		_trafficflow.updateTrafficFlow(trafficFlow);
 		
 		return Response.ok().build();
 	}
